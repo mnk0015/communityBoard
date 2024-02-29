dab71df7405bc18f4366a648572249b25e80c245
mnk0015 <mohammednhan0015@gmail.com>
2024-02-28 23:53:58 -0500

    added a cards component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Cards from './components/Cards'
 
 const App = () => {
 
   return (
     <div className="App">
-
+    <h1>Welcome to Community Board</h1>
+    <Cards/>
       
     </div>
   )
